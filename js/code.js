704584681626bcc15d9dcf3ea3e73658b825193c
Estarlin R <estarlincito@estarlincitoM.local>
2022-12-18 20:24:33 -0400

    0.12

--- js/code.js
+++ js/code.js
@@ -2,9 +2,9 @@ const menuID = document.getElementById("menu");
 
 //menu
 menuID.innerHTML = `
-  <div class="munu">
-  <a href="https://${window.location.host}/businesses">Home</a>
-  <a href="https://${window.location.host}/businesses/businesses/businesses.html">Businesses</a>
-  <a href="https://${window.location.host}/businesses/social-network/social-network.html">Social Network</a>
-  </div>
+  <nav class="menu">
+      <a href="https://${window.location.host}/businesses" class="menu__button">Home</a>
+      <a href="https://${window.location.host}/businesses/businesses/businesses.html" class="menu__button">Businesses</a>
+      <a href="https://${window.location.host}/businesses/social-network/social-network.html" class="menu__button">Social N</a>
+  </nav>
 `;
